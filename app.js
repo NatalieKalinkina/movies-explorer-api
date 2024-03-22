1d060ef432b3b2b6f9eaa099c9d4904d88fa3ec5
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2024-03-22 12:54:24 +0400

    fix: cors settings for local

--- app.js
+++ app.js
@@ -8,17 +8,15 @@ const routes = require("./routes/index");
 
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-// const { exceptionHandler } = require("./middlewares/exceptionHandler");
+const { exceptionHandler } = require("./middlewares/exceptionHandler");
 
-// const { rateLimiter } = require("./middlewares/rateLimiter");
+const { rateLimiter } = require("./middlewares/rateLimiter");
 
 const { MONGO_URL_DEV, PORT_DEV } = require("./constants");
 
 const { PORT, MONGO_URL, NODE_ENV } = process.env;
 
 const app = express();
-
-app.use(cors());
 
 app.use(function (req, res, next) {
   res.header(
@@ -34,8 +32,10 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(cors());
+
 app.use(helmet());
-// app.use(rateLimiter);
+app.use(rateLimiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -54,6 +54,6 @@ app.use(errorLogger);
 
 app.use(errors());
 
-// app.use(exceptionHandler);
+app.use(exceptionHandler);
 
 app.listen(NODE_ENV === "production" ? PORT : PORT_DEV);
